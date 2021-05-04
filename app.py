89eeab1610bd24bd9ed6c623cf52f7ddec403b8b
Mateusz <mateusz.milewski5.stud@pw.edu.pl>
2021-05-04 17:30:31 +0200

    almost done need small fixes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from Light import Light
 import sys
-import numpy
+import numpy as np
 import math
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
@@ -8,31 +8,55 @@ from typing import Tuple
 from PyQt5.QtWidgets import *
 from Ball import *
 
-MATERIAL_COLOR = Qt.red
-
 
 class Widget(QWidget):
     def __init__(self):
         self.screen_size = (800, 800)
         self.ballObject = Ball(240, 240, 0, 200)  # x, y, z, r
-        self.LightObject = Light(440, 440, 400, 20)  # x, y, z, power
+        self.LightObject = Light(440, 440, 400, 200)  # x, y, z, power
         super().__init__()
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        painter.setPen(QPen(MATERIAL_COLOR,  1, Qt.SolidLine))
-        painter.setBrush(QBrush(MATERIAL_COLOR, Qt.SolidPattern))
 
-        painter.drawEllipse(self.ballObject.x - self.ballObject.r, self.ballObject.y-self.ballObject.r,
-                            self.ballObject.r*2, self.ballObject.r*2)  # tutaj poczatek rysowania jest w lewym gornym rogu dlatego odejmowane tez odejmowana jest dlugosc promienia od srodka
-
-        painter.setPen(QPen(Qt.black,  1, Qt.SolidLine))
         for x in range(self.screen_size[0]):
             for y in range(self.screen_size[1]):
                 if not self.ballObject.isInBall(x, y):
+                    painter.setPen(QPen(Qt.black,  1, Qt.SolidLine))
                     painter.drawPoint(x, y)
                 else:
+                    z = self.ballObject.getZ(x, y)
 
+                    NormalVector = np.subtract([x, y, z], [
+                        self.ballObject.x, self.ballObject.y, self.ballObject.z])
+
+                    NormalNormalized = NormalVector / \
+                        np.linalg.norm(NormalVector)
+
+                    LightVector = np.subtract([x, y, z], [
+                        self.LightObject.x, self.LightObject.y, self.LightObject.z])
+
+                    LightNormalized = LightVector/np.linalg.norm(LightVector)
+
+                    ObservatorVector = [0, 0, 1]
+
+                    dot_product = np.dot(ObservatorVector, LightNormalized)
+                    angle = np.arccos(dot_product)
+
+                    Ia = 0
+                    Ip = self.LightObject.power
+                    Ka = 0.1
+                    Ks = 0.25
+                    Kd = 0.75
+                    fatt = 0.8
+                    n = 10
+                    final = (Ia * Ka) + (fatt * Ip*Kd *
+                                         np.dot(NormalNormalized, LightNormalized)) + \
+                        (fatt*Ip*Ks*math.cos(angle)**n)
+
+                    painter.setPen(
+                        QPen(QColor.fromHsv(342, 71, 79 - final),  1, Qt.SolidLine))
+                    painter.drawPoint(x, y)
 
                     # def keyPressEvent(self, event):
                     #     ROTATION_STEP = numpy.radians(0.8)
