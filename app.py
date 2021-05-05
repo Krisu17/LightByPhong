e2f223806dfa235cec7dbdb1cb09d0b4ef03ff40
Mateusz <mateusz.milewski5.stud@pw.edu.pl>
2021-05-05 13:58:29 +0200

    debuging sliders added

--- app.py
+++ app.py
@@ -11,11 +11,44 @@ from Ball import *
 
 class Widget(QWidget):
     def __init__(self):
-        self.screen_size = (400, 400)
-        self.ballObject = Ball(200, 200, 0, 100)  # x, y, z, r
+        self.screen_size = (200, 200)
+        self.ballObject = Ball(100, 100, 0, 50)  # x, y, z, r
         self.ballObject.setMaterialToMetal()
         self.LightObject = Light(440, 440, 400, 150)  # x, y, z, power
+
         super().__init__()
+        nSlider = QSlider(Qt.Horizontal, self)
+        nSlider.setGeometry(10, 10, 100, 10)
+        nSlider.valueChanged[int].connect(self.changeN)
+        self.labelN = QLabel('N=0', self)
+        self.labelN.setGeometry(110, 10, 50, 10)
+
+        ksSlider = QSlider(Qt.Horizontal, self)
+        ksSlider.setGeometry(10, 20, 100, 10)
+        ksSlider.valueChanged[int].connect(self.changeKs)
+        self.labelKs = QLabel('Ks=0', self)
+        self.labelKs.setGeometry(110, 20, 50, 10)
+
+        kdSlider = QSlider(Qt.Horizontal, self)
+        kdSlider.setGeometry(10, 30, 100, 10)
+        kdSlider.valueChanged[int].connect(self.changeKd)
+        self.labelKd = QLabel('Kd=0', self)
+        self.labelKd.setGeometry(110, 30, 50, 10)
+
+    def changeN(self, value):
+        self.ballObject.n = value
+        self.labelN.setText("N=" + str(self.ballObject.n))
+        self.update()
+
+    def changeKs(self, value):
+        self.ballObject.Ks = value/100
+        self.labelKs.setText("Ks=" + str(self.ballObject.Ks))
+        self.update()
+
+    def changeKd(self, value):
+        self.ballObject.Kd = value/100
+        self.labelKd.setText("Kd=" + str(self.ballObject.Kd))
+        self.update()
 
     def paintEvent(self, event):
         painter = QPainter(self)
@@ -34,8 +67,9 @@ class Widget(QWidget):
         for x in range(self.screen_size[0]):
             for y in range(self.screen_size[1]):
                 if not self.ballObject.isInBall(x, y):
-                    painter.setPen(QPen(Qt.black,  1, Qt.SolidLine))
-                    painter.drawPoint(x, y)
+                    # painter.setPen(QPen(Qt.black,  1, Qt.SolidLine))
+                    # painter.drawPoint(x, y)
+                    pass
                 else:
                     z = self.ballObject.getZ(x, y)
                     # fatt = math.sqrt(math.pow((x + self.LightObject.getX()), 2) + math.pow((y + self.LightObject.getY()), 2) + math.pow((z + self.LightObject.getZ()), 2))/3
@@ -99,35 +133,35 @@ class Widget(QWidget):
             self.update()
 
         # for debbuging
-        if event.key() == Qt.Key_I:
-            self.ballObject.increaseN()
-            print("N = ", self.ballObject.getN())
-            self.update()
-        if event.key() == Qt.Key_J:
-            self.ballObject.decreaseN()
-            print("N = ", self.ballObject.getN())
-            self.update()
-        if event.key() == Qt.Key_P:
-            self.ballObject.increaseKs()
-            print("Ks = ", self.ballObject.getKs())
-            self.update()
-        if event.key() == Qt.Key_L:
-            self.ballObject.decreaseKs()
-            print("Ks = ", self.ballObject.getKs())
-            self.update()
-        if event.key() == Qt.Key_O:
-            self.ballObject.increaseKd()
-            print("Kd = ", self.ballObject.getKd())
-            self.update()
-        if event.key() == Qt.Key_K:
-            self.ballObject.decreaseKd()
-            print("Kd = ", self.ballObject.getKd())
-            self.update()
+        # if event.key() == Qt.Key_I:
+        #     self.ballObject.increaseN()
+        #     print("N = ", self.ballObject.getN())
+        #     self.update()
+        # if event.key() == Qt.Key_J:
+        #     self.ballObject.decreaseN()
+        #     print("N = ", self.ballObject.getN())
+        #     self.update()
+        # if event.key() == Qt.Key_P:
+        #     self.ballObject.increaseKs()
+        #     print("Ks = ", self.ballObject.getKs())
+        #     self.update()
+        # if event.key() == Qt.Key_L:
+        #     self.ballObject.decreaseKs()
+        #     print("Ks = ", self.ballObject.getKs())
+        #     self.update()
+        # if event.key() == Qt.Key_O:
+        #     self.ballObject.increaseKd()
+        #     print("Kd = ", self.ballObject.getKd())
+        #     self.update()
+        # if event.key() == Qt.Key_K:
+        #     self.ballObject.decreaseKd()
+        #     print("Kd = ", self.ballObject.getKd())
+        #     self.update()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Widget()
-    ex.resize(400, 400)
+    ex.resize(200, 200)
     ex.show()
     sys.exit(app.exec_())
